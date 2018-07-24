4ffcc32edd54a2cfe2e3b68d23af700f324bf2cf
John Bogovic <bogovicj@janelia.hhmi.org>
2018-07-24 10:55:08 -0400

    update ImageStackLoader

--- src/main/java/bigwarp/loader/BigWarpImageStackImageLoader.java
+++ src/main/java/bigwarp/loader/BigWarpImageStackImageLoader.java
@@ -4,8 +4,8 @@ import ij.ImagePlus;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.function.Function;
 
-import bdv.ij.export.imgloader.ImagePlusImgLoader.SetupImgLoader;
 import mpicbg.spim.data.generic.sequence.BasicImgLoader;
 import mpicbg.spim.data.generic.sequence.BasicSetupImgLoader;
 import mpicbg.spim.data.generic.sequence.ImgLoaderHint;
@@ -18,6 +18,7 @@ import net.imglib2.img.basictypeaccess.array.IntArray;
 import net.imglib2.img.basictypeaccess.array.ShortArray;
 import net.imglib2.img.planar.PlanarImg;
 import net.imglib2.type.NativeType;
+import net.imglib2.type.NativeTypeFactory;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.NumericType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
@@ -26,16 +27,10 @@ import net.imglib2.type.numeric.real.FloatType;
 
 public abstract class BigWarpImageStackImageLoader< T extends NumericType< T > & NativeType< T >, A extends ArrayDataAccess< A > > implements BasicImgLoader, TypedBasicImgLoader< T >
 {
-	@SuppressWarnings( "unchecked" )
 	public static BigWarpImageStackImageLoader< UnsignedByteType, ByteArray > createUnsignedByteInstance( final ImagePlus imp, int[] ids )
 	{
-		return new BigWarpImageStackImageLoader< UnsignedByteType, ByteArray >( new UnsignedByteType(), imp, ids )
+		return new BigWarpImageStackImageLoader< UnsignedByteType, ByteArray >( new UnsignedByteType(), imp, array -> new ByteArray( ( byte[] ) array ), ids )
 		{
-			@Override
-			protected ByteArray wrapPixels( final Object array )
-			{
-				return new ByteArray( ( byte[] ) array );
-			}
 
 			@Override
 			protected void linkType( final PlanarImg< UnsignedByteType, ByteArray > img )
@@ -45,16 +40,10 @@ public abstract class BigWarpImageStackImageLoader< T extends NumericType< T > &
 		};
 	}
 
-	@SuppressWarnings( "unchecked" )
 	public static BigWarpImageStackImageLoader< UnsignedShortType, ShortArray > createUnsignedShortInstance( final ImagePlus imp, int[] ids )
 	{
-		return new BigWarpImageStackImageLoader< UnsignedShortType, ShortArray >( new UnsignedShortType(), imp, ids )
+		return new BigWarpImageStackImageLoader< UnsignedShortType, ShortArray >( new UnsignedShortType(), imp, array -> new ShortArray( ( short[] ) array ), ids )
 		{
-			@Override
-			protected ShortArray wrapPixels( final Object array )
-			{
-				return new ShortArray( ( short[] ) array );
-			}
 
 			@Override
 			protected void linkType( final PlanarImg< UnsignedShortType, ShortArray > img )
@@ -64,16 +53,10 @@ public abstract class BigWarpImageStackImageLoader< T extends NumericType< T > &
 		};
 	}
 
-	@SuppressWarnings( "unchecked" )
 	public static BigWarpImageStackImageLoader< FloatType, FloatArray > createFloatInstance( final ImagePlus imp, int[] ids )
 	{
-		return new BigWarpImageStackImageLoader< FloatType, FloatArray >( new FloatType(), imp, ids )
+		return new BigWarpImageStackImageLoader< FloatType, FloatArray >( new FloatType(), imp, array -> new FloatArray( ( float[] ) array ), ids )
 		{
-			@Override
-			protected FloatArray wrapPixels( final Object array )
-			{
-				return new FloatArray( ( float[] ) array );
-			}
 
 			@Override
 			protected void linkType( final PlanarImg< FloatType, FloatArray > img )
@@ -83,16 +66,10 @@ public abstract class BigWarpImageStackImageLoader< T extends NumericType< T > &
 		};
 	}
 
-	@SuppressWarnings( "unchecked" )
 	public static BigWarpImageStackImageLoader< ARGBType, IntArray > createARGBInstance( final ImagePlus imp, int[] ids )
 	{
-		return new BigWarpImageStackImageLoader< ARGBType, IntArray >( new ARGBType(), imp, ids )
+		return new BigWarpImageStackImageLoader< ARGBType, IntArray >( new ARGBType(), imp, array -> new IntArray( ( int[] ) array ), ids )
 		{
-			@Override
-			protected IntArray wrapPixels( final Object array )
-			{
-				return new IntArray( ( int[] ) array );
-			}
 
 			@Override
 			protected void linkType( final PlanarImg< ARGBType, IntArray > img )
@@ -110,50 +87,19 @@ public abstract class BigWarpImageStackImageLoader< T extends NumericType< T > &
 
 	private final HashMap< Integer, SetupImgLoader > setupImgLoaders;
 
-	public BigWarpImageStackImageLoader( final T type, final ImagePlus imp, int[] setupIds )
+	private final Function< Object, A > wrapPixels;
+
+	public BigWarpImageStackImageLoader( final T type, final ImagePlus imp,  final Function< Object, A > wrapPixels, int[] setupIds )
 	{
 		this.type = type;
 		this.imp = imp;
 		this.dim = new long[] { imp.getWidth(), imp.getHeight(), imp.getNSlices() };
 		final int numSetups = imp.getNChannels();
 		setupImgLoaders = new HashMap< Integer, SetupImgLoader >();
+		this.wrapPixels = wrapPixels;
 		for ( int i = 0; i < numSetups; ++i )
 			setupImgLoaders.put( setupIds[ i ], new SetupImgLoader( i ) );
 	}
-
-	public class SetupImgLoader implements BasicSetupImgLoader< T >
-	{
-		private final int channel;
-
-		public SetupImgLoader( final int channel )
-		{
-			this.channel = channel;
-		}
-
-		@Override
-		public RandomAccessibleInterval< T > getImage( final int timepointId, final ImgLoaderHint... hints )
-		{
-			return new PlanarImg< T, A >( dim, type.getEntitiesPerPixel() )
-			{
-				private PlanarImg< T, A > init()
-				{
-					final int frame = timepointId + 1;
-					for ( int slice = 1; slice <= dim[ 2 ]; ++slice )
-						mirror.set( slice - 1, wrapPixels( imp.getStack().getPixels( imp.getStackIndex( channel + 1, slice, frame ) ) ) );
-					linkType( this );
-					return this;
-				}
-			}.init();
-		}
-
-		@Override
-		public T getImageType()
-		{
-			return type;
-		}
-	}
-
-	protected abstract A wrapPixels( Object array );
 
 	protected abstract void linkType( PlanarImg< T, A > img );
 
@@ -161,5 +107,36 @@ public abstract class BigWarpImageStackImageLoader< T extends NumericType< T > &
 	public SetupImgLoader getSetupImgLoader( final int setupId )
 	{
 		return setupImgLoaders.get( setupId );
+	}
+	
+	public class SetupImgLoader implements BasicSetupImgLoader< T >
+	{
+		private final int setupId;
+
+		public SetupImgLoader( final int setupId )
+		{
+			this.setupId = setupId;
+		}
+
+		@Override
+		public RandomAccessibleInterval< T > getImage( final int timepointId, final ImgLoaderHint... hints )
+		{
+			final int channel = setupId + 1;
+			final int frame = timepointId + 1;
+			final ArrayList< A > slices = new ArrayList<>();
+			for ( int slice = 1; slice <= dim[ 2 ]; ++slice )
+				slices.add( wrapPixels.apply( imp.getStack().getPixels( imp.getStackIndex( channel, slice, frame ) ) ) );
+			final PlanarImg< T, A > img = new PlanarImg<>( slices, dim, type.getEntitiesPerPixel() );
+			@SuppressWarnings( "unchecked" )
+			final NativeTypeFactory< T, ? super A > typeFactory = ( NativeTypeFactory< T, ? super A > ) type.getNativeTypeFactory();
+			img.setLinkedType( typeFactory.createLinkedType( img ) );
+			return img;
+		}
+
+		@Override
+		public T getImageType()
+		{
+			return type;
+		}
 	}
 }
