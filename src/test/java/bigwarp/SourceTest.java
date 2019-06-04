4fb1cebf96ccb58582b864ed9f30fdea342f864f
John Bogovic <bogovicj@janelia.hhmi.org>
2019-06-04 09:36:10 -0400

    test that source works after transformation

--- src/test/java/bigwarp/SourceTest.java
+++ src/test/java/bigwarp/SourceTest.java
@@ -1,9 +1,11 @@
 package bigwarp;
 
+import java.io.File;
 import java.io.IOException;
 
 import bdv.export.ProgressWriterConsole;
 import bdv.gui.BigWarpViewerOptions;
+import bdv.tools.transformation.TransformedSource;
 import bdv.util.BdvFunctions;
 import bdv.util.BdvStackSource;
 import bdv.viewer.Source;
@@ -13,6 +15,7 @@ import ij.ImageJ;
 import ij.ImagePlus;
 import mpicbg.spim.data.SpimDataException;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 
 public class SourceTest
@@ -28,8 +31,17 @@ public class SourceTest
 				.show( ImageJFunctions.wrapByte( imp ), "mri-stack" );
 		Source< UnsignedByteType > src = bdv.getSources().get( 0 ).getSpimSource();
 
-		BigWarpData< UnsignedByteType > datasrc = BigWarpInit.createBigWarpData( new Source[] { src }, new Source[] { src }, new String[] { "mvg", "tgt" } );
-		BigWarp< UnsignedByteType > bw = new BigWarp<>( datasrc, "bw", BigWarpViewerOptions.options(), new ProgressWriterConsole() );
+		TransformedSource< UnsignedByteType > tsrc = new TransformedSource<>( src );
+		AffineTransform3D affine = new AffineTransform3D();
+		affine.set( 
+				0.2, 0.0, 0.0, 0.0,
+				0.0, 0.2, 0.0, 0.0,
+				0.0, 0.0, 0.4, 0.0 );
+		tsrc.setIncrementalTransform( affine );
 
+
+		BigWarpData< UnsignedByteType > datasrc = BigWarpInit.createBigWarpData( new Source[] { tsrc }, new Source[] { tsrc }, new String[] { "mvg", "tgt" } );
+		BigWarp< UnsignedByteType > bw = new BigWarp<>( datasrc, "bw", BigWarpViewerOptions.options(), new ProgressWriterConsole() );
+		bw.getLandmarkPanel().getTableModel().load( new File( "src/test/resources/mr_landmarks_p2p2p4-111.csv" ));
 	}
 }
