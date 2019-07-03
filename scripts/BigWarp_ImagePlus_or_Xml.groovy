c37b0872657606d79c31f59a83743b36ea0dab5c
John Bogovic <bogovicj@janelia.hhmi.org>
2019-07-03 12:05:08 -0400

    add doc message for xml/ip script.  clean up

--- scripts/BigWarp_ImagePlus_or_Xml.groovy
+++ scripts/BigWarp_ImagePlus_or_Xml.groovy
@@ -1,14 +1,9 @@
+#@ String (visibility=MESSAGE, value="<html>Run bigwarp with bigdataviewer xml-h5 files and/or images.<p> <p>XML files take precedence over open images.<p>&nbsp;&nbsp;i.e., if an xml file is given for either the moving (or target) source,<p>&nbsp;&nbsp;then the image that is specified for the moving (target) source will be ignored. </html>") DOCMSG
 #@ File(label="Moving xml", required=false, style="extensions:xml" ) movingXml
 #@ File(label="Target xml", required=false, style="extensions:xml" ) targetXml
 #@ ImagePlus(label="Moving image", autofill=true, required=false, description="Only used if moving xml is empty" ) movingIp
 #@ ImagePlus(label="Target image", autofill=true, required=false, description="Only used if target xml is empty" ) targetIp
 #@ File(label="Landmark file", required=false, style="extensions:csv") landmarksFile
-
-println( 'moving ip ' + movingIp )
-println( 'target ip ' + targetIp )
-
-println( 'moving xml ' + movingXml )
-println( 'target xml ' + targetXml )
 
 import java.lang.Exception;
 import bigwarp.BigWarp;
@@ -19,16 +14,12 @@ import bdv.export.ProgressWriterConsole;
 data = null;
 if( movingXml != null && targetXml != null  ){
     data = BigWarpInit.createBigWarpDataFromXML( movingXml.getAbsolutePath(), targetXml.getAbsolutePath());
-    println("xml xml")
 } else if( movingXml != null ){
     data = BigWarpInit.createBigWarpDataFromXMLImagePlus( movingXml.getAbsolutePath(), targetIp );
-    println("xml ip")
 } else if( targetXml != null ) {
     data = BigWarpInit.createBigWarpDataFromImagePlus( movingXml.getAbsolutePath(), targetIp );
-    println("ip xml")
 } else{
     data = BigWarpInit.createBigWarpDataFromImages( movingIp, targetIp);
-    println("ip ip")
 }
 
 
