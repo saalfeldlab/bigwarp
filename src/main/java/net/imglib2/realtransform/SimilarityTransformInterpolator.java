cc0f56d9c95b844bead66635b044015454f31087
bogovicj <bogovicj@janelia.hhmi.org>
2022-07-24 12:51:03 -0400

    feat: add different similarity transform interpolator
    * selected point takes a linear path from start to end

---
+++ src/main/java/net/imglib2/realtransform/SimilarityTransformInterpolator.java
@@ -0,0 +1,92 @@
+package net.imglib2.realtransform;
+
+import net.imglib2.util.LinAlgHelpers;
+
+import bdv.util.Affine3DHelpers;
+import bdv.viewer.animate.AbstractTransformAnimator;
+
+public class SimilarityTransformInterpolator extends AbstractTransformAnimator
+{
+	private final double[] qStart;
+
+	private final double[] qDiff;
+
+	private final double scaleStart;
+
+	private final double scaleEnd;
+
+	private final double scaleRate;
+
+	private final double[] p;
+
+	private final double[] pDiff;
+
+	public SimilarityTransformInterpolator(final AffineTransform3D transform, final double[] p) {
+		super(1);
+		AffineTransform3D transformEnd = new AffineTransform3D();
+		transformEnd.set(transform);
+
+		this.p = p;
+		qStart = new double[]{1, 0, 0, 0};
+		final double[] qStartInv = new double[4];
+		final double[] qEnd = new double[4];
+		final double[] qEndInv = new double[4];
+		qDiff = new double[4];
+		LinAlgHelpers.quaternionInvert(qStart, qStartInv);
+
+		Affine3DHelpers.extractRotation(transformEnd, qEnd);
+		LinAlgHelpers.quaternionInvert(qEnd, qEndInv);
+
+		LinAlgHelpers.quaternionMultiply(qStartInv, qEnd, qDiff);
+		if (qDiff[0] < 0)
+			LinAlgHelpers.scale(qDiff, -1, qDiff);
+
+		scaleStart = 1.0;
+		scaleEnd = Affine3DHelpers.extractScale(transformEnd, 0);
+		scaleRate = scaleEnd / scaleStart;
+
+		// translation needed to from reconstructed to target transformation
+		pDiff = new double[3];
+		double[] tmp = new double[3];
+		get(1).apply( p, tmp);
+		transform.apply(p, pDiff);
+		LinAlgHelpers.subtract(pDiff, tmp, pDiff);
+	}
+	
+	@Override
+	public AffineTransform3D get(final double t) {
+
+		final double[] qDiffCurrent = new double[4];
+		final double[] qCurrent = new double[4];
+		LinAlgHelpers.quaternionPower(qDiff, t, qDiffCurrent);
+		LinAlgHelpers.quaternionMultiply(qStart, qDiffCurrent, qCurrent);
+
+		final double alpha = Math.pow(scaleRate, t);
+		final double scaleCurrent = scaleStart * alpha;
+
+		final double[][] Rcurrent = new double[3][3];
+		LinAlgHelpers.quaternionToR(qCurrent, Rcurrent);
+
+		final double[][] m = new double[3][4];
+		for (int r = 0; r < 3; ++r) {
+			for (int c = 0; c < 3; ++c)
+				m[r][c] = scaleCurrent * Rcurrent[r][c];
+		}
+
+		final AffineTransform3D transform = new AffineTransform3D();
+		transform.set(m);
+
+		double[] pCurrent = new double[3];
+		transform.apply(p, pCurrent);
+
+		double[] pTgt = new double[3];
+		LinAlgHelpers.scale( pDiff, t, pTgt );
+		LinAlgHelpers.add( p, pTgt, pTgt );
+		LinAlgHelpers.subtract( pTgt, pCurrent, pTgt );
+		transform.translate( pTgt );
+
+		return transform;
+
+	}
+
+}
