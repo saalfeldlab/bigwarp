641362610babfb26a40aa26af37b5029445d4414
Caleb Hulbert <cmhulbert@users.noreply.github.com>
2022-09-02 15:17:16 -0400

    feat: make Bookmark non-final

--- src/main/java/bigwarp/BigWarp.java
+++ src/main/java/bigwarp/BigWarp.java
@@ -220,7 +220,7 @@ public class BigWarp< T >
 
 	final AffineTransform3D fixedViewXfm;
 
-	private final Bookmarks bookmarks;
+	private Bookmarks bookmarks;
 
 	protected final BookmarksEditor bookmarkEditorP;
 
@@ -2013,6 +2013,11 @@ public class BigWarp< T >
 	{
 		this.movingSpimData = movingSpimData;
 		this.movingImageXml = movingImageXml;
+	}
+
+	public void setBookmarks( final Bookmarks bookmarks )
+	{
+		this.bookmarks = bookmarks;
 	}
 
 	public enum WarpVisType
