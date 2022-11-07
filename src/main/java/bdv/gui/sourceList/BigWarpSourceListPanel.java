046cf05d3f4428fd5454454d58a9fc6e8c31b653
John Bogovic <bogovicj@janelia.hhmi.org>
2022-11-07 09:25:29 -0500

    feat: start new init dialog
    * depend on flatlaf

---
+++ src/main/java/bdv/gui/sourceList/BigWarpSourceListPanel.java
@@ -0,0 +1,104 @@
+/*-
+ * #%L
+ * BigWarp plugin for Fiji.
+ * %%
+ * Copyright (C) 2015 - 2022 Howard Hughes Medical Institute.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+package bdv.gui.sourceList;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.table.TableCellRenderer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import bdv.gui.sourceList.BigWarpSourceTableModel.ButtonEditor;
+import bdv.gui.sourceList.BigWarpSourceTableModel.ButtonRenderer;
+import bdv.gui.sourceList.BigWarpSourceTableModel.RemoveRowButton;
+
+public class BigWarpSourceListPanel extends JPanel
+{
+
+	private static final long serialVersionUID = 2370565900502584680L;
+
+	protected BigWarpSourceTableModel tableModel;
+
+	protected JTable table;
+
+	public final Logger logger = LoggerFactory.getLogger( BigWarpSourceListPanel.class );
+
+//	public BigWarpSourceListPanel()
+	public BigWarpSourceListPanel( BigWarpSourceTableModel tableModel )
+	{
+		super( new GridLayout( 1, 0 ) );
+		setTableModel( tableModel );
+
+		table = new JTable( tableModel );
+		table.setPreferredScrollableViewportSize( new Dimension( 500, 70 ) );
+		table.setFillsViewportHeight( true );
+
+		table.getColumn( "remove" ).setCellRenderer( new ButtonRenderer() );
+		table.getColumn( "remove" ).setCellEditor( new ButtonEditor( new JCheckBox(), tableModel ) );
+
+		final JScrollPane scrollPane = new JScrollPane( table );
+		add( scrollPane );
+	}
+
+	public BigWarpSourceTableModel getTableModel()
+	{
+		return tableModel;
+	}
+
+	public void genJTable()
+	{
+		table = new JTable( getTableModel() );
+
+		table.setPreferredScrollableViewportSize( new Dimension( 400, 800 ) );
+		table.setFillsViewportHeight( true );
+		table.setShowVerticalLines( false );
+
+//		table.setDefaultEditor( String.class,
+//				new TextFieldCellEditor( new TextFieldCell(table), String.class ));
+	}
+
+	public void setTableModel( BigWarpSourceTableModel tableModel )
+	{
+		this.tableModel = tableModel;
+		genJTable();
+	}
+
+	public JTable getJTable()
+	{
+		return table;
+	}
+
+
+}
---
+++ src/main/java/bdv/gui/sourceList/BigWarpSourceTableModel.java
@@ -0,0 +1,290 @@
+package bdv.gui.sourceList;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+
+public class BigWarpSourceTableModel extends AbstractTableModel 
+{
+	private static final long serialVersionUID = 5923947651732788341L;
+
+	protected final static String[] colNames = new String[] { "Name", "Moving", "Transform", "remove" };
+
+	protected final String[] columnNames;
+	protected final ArrayList<SourceRow> sources;
+	protected final ArrayList<RemoveRowButton> rmRowButtons;
+
+	private Component container;
+
+	public BigWarpSourceTableModel()
+	{
+		super();
+		columnNames = colNames;
+		sources = new ArrayList<>();
+		rmRowButtons = new ArrayList<>();
+	}
+	
+	/**
+	 * Set the {@link Component} to repaint when a row is removed.
+	 * 
+	 * @param container the component containing this table
+	 */
+	public void setContainer( Component container )
+	{
+		this.container = container;
+	}
+
+	@Override 
+	public String getColumnName( int col ){
+		return columnNames[col];
+	}
+
+	@Override
+	public int getColumnCount()
+	{
+		return columnNames.length;
+	}
+
+	@Override
+	public int getRowCount()
+	{
+		return sources.size();
+	}
+
+	@Override
+	public Object getValueAt( int r, int c )
+	{
+		if( c == 3 )
+			return rmRowButtons.get( r );
+		else
+			return sources.get( r ).get( c );
+	}
+
+	public Class<?> getColumnClass( int col ){
+		if ( col == 1 )
+			return Boolean.class;
+		else if ( col == 3 )
+			return JButton.class;
+		else
+			return String.class;
+	}
+
+	@Override
+	public boolean isCellEditable( int row, int col )
+	{
+		return true;
+	}
+
+	public void add( String srcName, boolean moving, boolean isImagePlus )
+	{
+		final RemoveRowButton rmButton = new RemoveRowButton( sources.size() );
+//		rmButton.addActionListener( e -> {
+//			System.out.println( "pushed rm row button " + rmButton.getRow() );
+//			remove( rmButton.getRow() );
+//		});
+
+		rmRowButtons.add( rmButton );
+		sources.add( new SourceRow( srcName, moving, "", isImagePlus ));
+	}
+
+	public void add( String srcName, boolean moving )
+	{
+		add( srcName, moving, false );
+	}
+
+	public void add( String srcName )
+	{
+		add( srcName, false );
+	}
+
+	public boolean remove( int i )
+	{
+		System.out.println( "rm     " + i );
+		System.out.println( "src sz " + sources.size() );
+		if( i >= sources.size() )
+		{
+			System.out.println( "not removed " );
+			return false;
+		}
+
+		sources.remove( i );
+		rmRowButtons.remove( i );
+		updateRmButtonIndexes();
+
+		if( container != null )
+			container.repaint();
+
+		return true;
+	}
+
+	private void updateRmButtonIndexes()
+	{
+		for( int i = 0; i < rmRowButtons.size(); i++ )
+			rmRowButtons.get( i ).setRow( i );
+	}
+
+	public static class SourceRow
+	{
+		public String srcName;
+		public boolean moving;
+		public String transformName;
+		
+		public boolean isImagePlus;
+		
+		public SourceRow( String srcName, boolean moving, String transformName, boolean isImagePlus )
+		{
+			this.srcName = srcName;
+			this.moving = moving;
+			this.transformName = transformName;
+			this.isImagePlus = isImagePlus;
+		}
+
+		public SourceRow( String srcName, boolean moving, String transformName )
+		{
+			this( srcName, moving, transformName, false );
+		}
+
+		public Object get( int c )
+		{
+			if( c == 0 )
+				return srcName;
+			else if( c == 1 )
+				return moving;
+			else if ( c == 2 )
+				return transformName;
+			else
+				return null;
+		}
+	}
+
+	protected static class RemoveRowButton extends JButton {
+		private int row;
+		public RemoveRowButton( int row )
+		{
+			super( "-" );
+			setRow( row );
+		}
+
+		public int getRow()
+		{
+			return row;
+		}
+
+		public void setRow(int row)
+		{
+			this.row = row;
+		}
+	}
+
+	/**
+	 * From 
+	 * http://www.java2s.com/Code/Java/Swing-Components/ButtonTableExample.htm
+	 */
+	protected static class ButtonRenderer extends JButton implements TableCellRenderer
+	{
+		public ButtonRenderer()
+		{
+			setOpaque( true );
+		}
+
+		public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column )
+		{
+			if ( isSelected )
+			{
+				setForeground( table.getSelectionForeground() );
+				setBackground( table.getSelectionBackground() );
+			}
+			else
+			{
+				setForeground( table.getForeground() );
+				setBackground( UIManager.getColor( "Button.background" ) );
+			}
+			setText( ( value == null ) ? "" : ((RemoveRowButton)value).getText());
+			return this;
+		}
+	}
+
+	/**
+	 * From 
+	 * http://www.java2s.com/Code/Java/Swing-Components/ButtonTableExample.htm
+	 */
+	protected static class ButtonEditor extends DefaultCellEditor
+	{
+		protected JButton button;
+
+		private String label;
+		
+		private RemoveRowButton thisButton;
+
+		private BigWarpSourceTableModel model;
+
+		private boolean isPushed;
+
+		public ButtonEditor( JCheckBox checkBox, BigWarpSourceTableModel model )
+		{
+			super( checkBox );
+			checkBox.setText( "-" );
+			this.model = model;
+
+			button = new JButton();
+			button.setOpaque( true );
+			button.addActionListener( new ActionListener()
+			{
+				public void actionPerformed( ActionEvent e )
+				{
+					fireEditingStopped();
+				}
+			} );
+		}
+
+		public Component getTableCellEditorComponent( JTable table, Object value, boolean isSelected, int row, int column )
+		{
+			if ( isSelected )
+			{
+				button.setForeground( table.getSelectionForeground() );
+				button.setBackground( table.getSelectionBackground() );
+			}
+			else
+			{
+				button.setForeground( table.getForeground() );
+				button.setBackground( table.getBackground() );
+			}
+			thisButton = ((RemoveRowButton)value);
+			label = ( value == null ) ? "" : thisButton.getText();
+			button.setText( label );
+			isPushed = true;
+			return button;
+		}
+
+		public Object getCellEditorValue()
+		{
+			if ( isPushed )
+			{
+				 model.remove( thisButton.getRow() );
+			}
+			isPushed = false;
+			return new String( label );
+		}
+
+		public boolean stopCellEditing()
+		{
+			isPushed = false;
+			return super.stopCellEditing();
+		}
+
+		protected void fireEditingStopped()
+		{
+			super.fireEditingStopped();
+		}
+	}
+
+}
