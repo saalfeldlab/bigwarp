f20ef0bb5e10b6e849e6354ab2181ea47cea1562
John Bogovic <bogovicj@janelia.hhmi.org>
2022-11-16 17:20:56 -0500

    better BigWarpCommand example

--- src/main/java/bdv/ij/BigWarpCommand.java
+++ src/main/java/bdv/ij/BigWarpCommand.java
@@ -1,9 +1,12 @@
 package bdv.ij;
 
+import java.io.IOException;
+
 import org.scijava.command.Command;
 import org.scijava.plugin.Plugin;
 
 import bdv.gui.BigWarpInitDialog;
+import ij.IJ;
 import net.imagej.ImageJ;
 
 @Plugin(type = Command.class, menuPath = "Plugins>BigDataViewer>Big Warp Command" )
@@ -15,10 +18,16 @@ public class BigWarpCommand implements Command
 		BigWarpInitDialog.createAndShow();
 	}
 	
-	public static void main( String[] args )
+	public static void main( String[] args ) throws IOException
 	{
 		ImageJ ij2 = new ImageJ();
 		ij2.ui().showUI();
+
+		Object im1 = ij2.io().open( "/home/john/tmp/mri-stack.tif" );
+		Object im2 = ij2.io().open( "/home/john/tmp/t1-head.tif" );
+
+		ij2.ui().show( im1 );
+		ij2.ui().show( im2 );
 	}
 
 }
