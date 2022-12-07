6ea15774bbde65b01ea90ecc605bc5bb3540f62d
John Bogovic <bogovicj@janelia.hhmi.org>
2022-12-07 16:17:59 -0500

    fix: better intervals for warp vis sources

--- src/main/java/bigwarp/source/JacobianDeterminantSource.java
+++ src/main/java/bigwarp/source/JacobianDeterminantSource.java
@@ -35,6 +35,7 @@ import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealRandomAccess;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.realtransform.BoundingBoxEstimation;
 import net.imglib2.realtransform.inverse.DifferentiableRealTransform;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
@@ -62,7 +63,13 @@ public class JacobianDeterminantSource< T extends RealType< T >> implements Sour
 
 		//RandomAccessibleInterval<?> fixedsrc = sourceData.sources.get( 1 ).getSpimSource().getSource( 0, 0 );
 //		interval = sourceData.sources.get( sourceData.targetSourceIndices[ 0 ] ).getSpimSource().getSource( 0, 0 );
-		interval = sourceData.getTargetSource( 0 ).getSpimSource().getSource( 0, 0 );
+//		interval = sourceData.getTargetSource( 0 ).getSpimSource().getSource( 0, 0 );
+
+		final BoundingBoxEstimation bbe = new BoundingBoxEstimation();
+		final AffineTransform3D affine = new AffineTransform3D();
+		data.getTargetSource( 0 ).getSpimSource().getSourceTransform( 0, 0, affine );
+		interval = bbe.estimatePixelInterval(  affine, data.getTargetSource( 0 ).getSpimSource().getSource( 0, 0 ) );
+
 
 //		VoxelDimensions srcVoxDims = sourceData.sources.get( sourceData.targetSourceIndices[ 0 ] ).getSpimSource().getVoxelDimensions();
 		VoxelDimensions srcVoxDims = sourceData.getTargetSource( 0 ).getSpimSource().getVoxelDimensions();
--- src/main/java/bigwarp/source/WarpMagnitudeSource.java
+++ src/main/java/bigwarp/source/WarpMagnitudeSource.java
@@ -35,6 +35,7 @@ import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealRandomAccess;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.realtransform.BoundingBoxEstimation;
 import net.imglib2.realtransform.RealTransform;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
@@ -61,7 +62,13 @@ public class WarpMagnitudeSource< T extends RealType< T >> implements Source< T 
 		sourceData = data;
 
 //		interval = sourceData.sources.get( sourceData.targetSourceIndices[ 0 ] ).getSpimSource().getSource( 0, 0 );
-		interval = sourceData.getTargetSource( 0 ).getSpimSource().getSource( 0, 0 );
+//		interval = sourceData.getTargetSource( 0 ).getSpimSource().getSource( 0, 0 );
+
+		final BoundingBoxEstimation bbe = new BoundingBoxEstimation();
+		final AffineTransform3D affine = new AffineTransform3D();
+		data.getTargetSource( 0 ).getSpimSource().getSourceTransform( 0, 0, affine );
+		interval = bbe.estimatePixelInterval(  affine, data.getTargetSource( 0 ).getSpimSource().getSource( 0, 0 ) );
+
 
 //		VoxelDimensions srcVoxDims = sourceData.sources.get( sourceData.targetSourceIndices[ 0 ] ).getSpimSource().getVoxelDimensions();
 		final VoxelDimensions srcVoxDims = sourceData.getTargetSource( 0 ).getSpimSource().getVoxelDimensions();
