90a4f45e23052e3bf23f6605673d2a3bb0a7f482
John Bogovic <bogovicj@janelia.hhmi.org>
2024-09-27 13:52:30 -0400

    test: more helpers in BigWarpTestUtils

--- src/test/java/bigwarp/BigWarpTestUtils.java
+++ src/test/java/bigwarp/BigWarpTestUtils.java
@@ -1,20 +1,5 @@
 package bigwarp;
 
-import bdv.gui.BigWarpViewerOptions;
-import bdv.viewer.Source;
-import bigwarp.source.SourceInfo;
-import com.google.common.collect.MapDifference;
-import com.google.common.collect.Maps;
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import ij.IJ;
-import ij.ImagePlus;
-import ij.gui.NewImage;
-import ij.plugin.StackWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.lang.reflect.Type;
@@ -25,14 +10,42 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.DoubleStream;
+
+import org.bouncycastle.util.Arrays;
+import org.junit.Assert;
+
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import bdv.gui.BigWarpViewerOptions;
+import bdv.viewer.Source;
+import bigwarp.landmarks.LandmarkTableModel;
+import bigwarp.source.SourceInfo;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.NewImage;
+import ij.plugin.StackWriter;
 import mpicbg.spim.data.SpimDataException;
 import net.imglib2.FinalInterval;
+import net.imglib2.RealPoint;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.ByteArray;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.iterator.IntervalIterator;
 import net.imglib2.position.FunctionRandomAccessible;
+import net.imglib2.realtransform.RealTransform;
+import net.imglib2.realtransform.Scale;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.view.Views;
-import org.junit.Assert;
 
 public class BigWarpTestUtils
 {
@@ -259,7 +272,7 @@ public class BigWarpTestUtils
 		return new BigWarp<>( data, opts, null );
 	}
 
-	static ImagePlus generateImagePlus( final String title )
+	public static ImagePlus generateImagePlus( final String title )
 	{
 		final FunctionRandomAccessible< UnsignedByteType > fimg = new FunctionRandomAccessible<>(
 				3,
@@ -268,8 +281,107 @@ public class BigWarpTestUtils
 		return ImageJFunctions.wrap( Views.interval( fimg, new FinalInterval( 32, 32, 1 ) ), title );
 	}
 
-	public static void main( String[] args ) throws SpimDataException, URISyntaxException, IOException
-	{
-		createBigWarp( true, true, false, false);
+	public static ImagePlus generateImagePlus3d(final String title,
+			final long[] size,
+			final long[] pos,
+			final int value,
+			final double[] resolution,
+			final double[] offset) {
+
+		final ArrayImg<UnsignedByteType, ByteArray> img = ArrayImgs.unsignedBytes(size);
+		img.getAt(pos).set(value);
+
+		final ImagePlus imp = ImageJFunctions.wrap(img, title);
+		imp.setDimensions(1, (int)size[2], 1);
+
+		imp.getCalibration().pixelWidth = resolution[0];
+		imp.getCalibration().pixelHeight = resolution[1];
+		imp.getCalibration().pixelDepth = resolution[2];
+
+		imp.getCalibration().xOrigin = offset[0];
+		imp.getCalibration().yOrigin = offset[1];
+		imp.getCalibration().zOrigin = offset[2];
+
+		return imp;
 	}
+
+	public static class TestImagePlusBuilder {
+
+		String title = "img";
+		long[] size = new long[]{32, 16, 8};
+		long[] pos = new long[]{16, 8, 4};
+		int value = 1;
+		double[] resolution = new double[]{4, 3, 2};
+		double[] offset = new double[]{0, 0, 0};
+
+		public ImagePlus build() {
+
+			return generateImagePlus3d(title, size, pos, value, resolution, offset);
+		}
+
+		public TestImagePlusBuilder title(String title) {
+
+			this.title = title;
+			return this;
+		}
+
+		public TestImagePlusBuilder size(long[] size) {
+
+			this.size = size;
+			return this;
+		}
+
+		public TestImagePlusBuilder position(long[] pos) {
+
+			this.pos = pos;
+			return this;
+		}
+
+		public TestImagePlusBuilder position(int value) {
+
+			this.value = value;
+			return this;
+		}
+
+		public TestImagePlusBuilder resolution(double[] resolution) {
+
+			this.resolution = resolution;
+			return this;
+		}
+
+		public TestImagePlusBuilder offset(double[] offset) {
+
+			this.offset = offset;
+			return this;
+		}
+
+	}
+
+	public static LandmarkTableModel identityLandmarks(int nd) {
+
+		final long[] min = new long[nd];
+		Arrays.fill(min, -1);
+
+		final long[] max = new long[nd];
+		Arrays.fill(max, 1);
+
+		final double[] ones = DoubleStream.generate(() -> 1.0).limit(nd).toArray();
+		return landmarks(new IntervalIterator(min, max), new Scale(ones));
+	}
+
+	public static LandmarkTableModel landmarks(final IntervalIterator it, RealTransform tform) {
+
+		final int nd = it.numDimensions();
+		final LandmarkTableModel ltm = new LandmarkTableModel(nd);
+
+		final RealPoint pt = new RealPoint(nd);
+		while (it.hasNext()) {
+			it.fwd();
+			tform.apply(it, pt);
+			ltm.add(it.positionAsDoubleArray(), pt.positionAsDoubleArray());
+		}
+
+		return ltm;
+	}
+
 }
